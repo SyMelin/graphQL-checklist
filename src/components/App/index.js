8224cdbcf5ff5a2ecea25a7dcdc8bcce75ebbff0
Sylvie Melin <sy.melin@gmail.com>
2023-01-29 19:09:13 +0100

    toggleTodo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,4 @@
-import { useQuery, gql } from '@apollo/client';
+import { useQuery, gql, useMutation } from '@apollo/client';
 import './App.css';
 
 const GET_TODOS = gql`
@@ -11,13 +11,30 @@ const GET_TODOS = gql`
   }
 `;
 
+const TOGGLE_TODO = gql `
+  mutation toggleTodo($id: uuid!, $done: Boolean!) {
+    update_todos(where: {id: {_eq: $id }}, _set: {done: $done}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
+
 //list todos
 //add todos
 //toggle todos
 //delete todos
 
 function App() {
-  const { loading, error, data } = useQuery(GET_TODOS);
+  const { loading, error, data } = useQuery(GET_TODOS)
+  const [toggleTodo] = useMutation(TOGGLE_TODO)
+
+  async function handleToggleTodo({ id, done }) {
+    await toggleTodo({ variables: { id: id, done: !done }})
+  }
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
@@ -51,6 +68,7 @@ function App() {
             <input
               className="todo__checkbox"
               type="checkbox"
+              onClick={() => handleToggleTodo(todo)}
               //checked={todo.done}
             />
             <p className="todo__text">
