66737a1f0c10375dc02b36a60f8e87259f3ca6c9
Sylvie Melin <sy.melin@gmail.com>
2023-01-29 12:46:18 +0100

    ApolloProvider

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+//list todos
+//add todos
+//toggle todos
+//delete todos
+
+function App() {
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './components/App';
+import './styles/index.css';
+
 import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/', //uri to be changed
+  cache: new InMemoryCache(),
+  connectToDevTools: true, //To enable the devtools in your app in production
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
