6ab4257b5c7ec266b2217d1ccd105a87c4dd89d3
Sylvie Melin <sy.melin@gmail.com>
2023-01-30 16:17:53 +0100

    addTodo mutation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,7 @@
-import { useQuery, gql, useMutation } from '@apollo/client';
+import { useState } from 'react'
+import { useQuery, gql, useMutation } from '@apollo/client'
 import './App.css';
+import React from 'react';
 
 const GET_TODOS = gql`
   query getTodos {
@@ -9,7 +11,7 @@ const GET_TODOS = gql`
       text
     }
   }
-`;
+`
 
 const TOGGLE_TODO = gql `
   mutation toggleTodo($id: uuid!, $done: Boolean!) {
@@ -23,17 +25,43 @@ const TOGGLE_TODO = gql `
   }
 `
 
-//list todos
-//add todos
-//toggle todos
-//delete todos
+const ADD_TODO = gql `
+  mutation addTodo($text: String!) {
+    insert_todos(objects: {text: $text}) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
 
 function App() {
+  const [todoText, setTodoText] =  useState("")
+
   const { loading, error, data } = useQuery(GET_TODOS)
   const [toggleTodo] = useMutation(TOGGLE_TODO)
+  const [addTodo] = useMutation(ADD_TODO, {
+    onCompleted: () =>  setTodoText('')
+  })
 
   async function handleToggleTodo({ id, done }) {
-    await toggleTodo({ variables: { id: id, done: !done }})
+    const data = await toggleTodo({ variables: { id: id, done: !done }})
+    console.log("toggled todo", data)
+  }
+
+  async function handleAddTodo(e) {
+    e.preventDefault()
+    if (!todoText.trim()) return
+    const data = await addTodo({
+      variables: { text: todoText },
+      refetchQueries: [
+        { query: GET_TODOS } //execute a query after addTodo mutation is done
+      ]
+  })
+    console.log("added todo", data)
+    {/* setTodoText('') */}
   }
 
   if (loading) return <p>Loading...</p>;
@@ -45,12 +73,17 @@ function App() {
     <div className="App">
       <h1 className="App__title">GraphQL Checklist</h1>
       {/*Todo form */}
-      <form className="form">
+      <form
+        className="form"
+        onSubmit={handleAddTodo}
+      >
         <h1 className="form__title">New Task</h1>
         <input
           className="form__input"
           type="text"
           placeholder="Write a new task"
+          onChange={(e) => setTodoText(e.target.value)}
+          value={todoText}
         />
         <button
           className="form__button--submit"
@@ -77,12 +110,10 @@ function App() {
                 type="checkbox"
                 name={todo.id}
                 onClick={() => handleToggleTodo(todo)}
-                //checked={todo.done}
+                defaultChecked={todo.done}
               />
               <p className="todo__text">{todo.text}</p>
-             {/* <span className="todo__checkLine"></span>*/} 
             </label>
-            
             <button
               className="todo__button--delete"
               type="button"
@@ -93,7 +124,7 @@ function App() {
       ))}
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
